c07b0acfbab95759b888e6f0c3afea7f874c809e
Aishwarya Khedkar <160451524+aishwaryaK2024@users.noreply.github.com>
2025-07-23 18:20:43 +0530

    Add files via upload

---
+++ dTSize.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    printf("Size of int = %d\n",sizeof(int));
+    printf("Size of float = %d\n",sizeof(float));
+    printf("Size of double = %d\n",sizeof(double));
+    printf("Size of char = %d\n",sizeof(char));
+   // printf("Size of bool = %d",sizeof(bool));
+    printf("Size of long = %d\n",sizeof(int));
+    printf("Size of long long int = %d\n",sizeof(long long int));
+    printf("Size of unsigned long long int = %d\n",sizeof(unsigned long long int));
+}
