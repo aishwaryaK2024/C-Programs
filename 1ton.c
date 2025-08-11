6cedb6e12ead2e1e425712be1ad8fe05f69765d0
Aishwarya Khedkar <160451524+aishwaryaK2024@users.noreply.github.com>
2025-08-11 22:02:56 +0530

    Add files via upload

---
+++ 1ton.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+
+    int n = 0,i = 1;
+    printf("Enter n = ");
+    scanf("%d",&n);
+    while(i<=n){
+        printf("%d\n",i);
+        i++;
+    }
+
+    return 0;
+}
