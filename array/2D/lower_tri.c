facfe077d30d919d5d14bafe5894cf3b45017c9f
aishwaryaK2024 <akhedkar522@gmail.com>
2025-10-08 19:13:47 +0530

    Lower Trianguler

---
+++ array/2D/lower_tri.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main(){
+    int n = 3;
+    int row=0,col=0;
+    int A[n][n];
+    printf("Enter row elements : \n");
+    for(row=0;row<n;row++){
+        for(col=0;col<n;col++){
+            scanf("%d",&A[row][col]);
+        }
+    }
+    for(row=0;row<n;row++){
+        for(col=0;col<n;col++){
+            printf("%d ",A[row][col]);
+        }
+        printf("\n");
+    }
+    printf("Lower Trianguler : \n");
+    for(row=0;row<n;row++){
+        for(col=0;col<n;col++){
+            if(col<row){
+                printf("%d ",A[row][col]);
+            }
+        }
+    }
+    return 0;
+}
