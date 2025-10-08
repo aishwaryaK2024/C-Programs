85b5dde6807484595922c592fd92b544ef3691a6
aishwaryaK2024 <akhedkar522@gmail.com>
2025-10-08 18:33:04 +0530

    2d.c

--- array/2D/2d.c
+++ array/2D/2d.c
@@ -1,21 +1,22 @@
 #include<stdio.h>
 
 int main(){
-    int A[3][3];
-    int row = 0,col = 0;
+    int n=0,m=0;//n=row ,m=col
+    int row=0,col=0;
+    printf("Enter number of row and col (nxm) of matrix : ");
+    scanf("%d %d",&n,&m);
+    int A[n][m];
     printf("Enter Matrix Elements");
-    for(row=0;row<3;row++){
-        for(col=0;col<3;col++){
+    for(row=0;row<n;row++){
+        for(col=0;col<m;col++){
             scanf("%d",&A[row][col]);
         }
     }
-    for(row=0;row<3;row++){
-        for(col=0;col<3;col++){
+    for(row=0;row<n;row++){
+        for(col=0;col<m;col++){
             printf("%d ",A[row][col]);
         }
         printf("\n");
     }
-
-    
     return 0;
 }
