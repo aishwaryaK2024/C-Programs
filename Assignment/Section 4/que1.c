aff7dd35e1378f1ace8e1dc70500dc6a08535d1a
Aishwarya Khedkar <160451524+aishwaryaK2024@users.noreply.github.com>
2025-10-27 23:35:45 +0530

    Add files via upload

---
+++ Assignment/Section 4/que1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int n = 0,i = 0;
+    printf("Enter n = ");
+    scanf("%d",&n);
+
+    for(i=1;i<=n;i++){
+        if(i%2==0)
+            printf("%d\n",i);
+    }
+    return 0;
+}
