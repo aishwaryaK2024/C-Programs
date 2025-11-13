a8e98ffb457e0449c0ddae2dfd7e6d7edc578dd6
Aishwarya Khedkar <160451524+aishwaryaK2024@users.noreply.github.com>
2025-11-13 08:24:57 +0530

    Structure

---
+++ prog2.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+struct student
+{
+    int rollNo;
+    char name[50];
+    int contactNo;
+    int class;
+    char address[200];
+    char email[50];
+    double marks[5];
+};
+
+int main(){
+
+    struct student Alice;
+    Alice.rollNo = 1;
+    Alice.name = "Alice";
+    Alice.contactNo = 1234567896;
+    Alice.class = 10;
+    Alice.address = "pune";
+    Alice.marks[0] = 89;
+    Alice.marks[1] = 90;
+    Alice.marks[2] = 80;
+    Alice.marks[3] = 96;
+    Alice.marks[4] = 82;
+
+    printf("Student Details : ");
+    printf("%d ",Alice.contactNo);
+    return 0;
+}
