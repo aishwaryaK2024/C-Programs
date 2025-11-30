b54d8d095667e56c48783c10a8f2d244d54efaa7
Aishwarya Khedkar <160451524+aishwaryaK2024@users.noreply.github.com>
2025-11-30 23:19:57 +0530

    que7.c

---
+++ Assignment/Section 4/que7.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    int n = 0,cnt = 0,r = 0,n1 = 0;
+    printf("Enter n = ");
+    scanf("%d",&n);
+    n1 = n;
+    while(n>0){
+        r = n%10;
+        ++cnt;
+        n = n/10;
+    }
+    printf("Number of Digits in %d = %d",n1,cnt);
+    return 0;
+}
