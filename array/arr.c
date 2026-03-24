3f9a3310fed73cef32af609984b27e8dbf3aed5f
Aishwarya Khedkar <160451524+aishwaryaK2024@users.noreply.github.com>
2026-03-24 23:45:12 +0530

    Updated

--- array/arr.c
+++ array/arr.c
@@ -4,14 +4,14 @@ int main() {
     int n;
     printf("Enter number of elements: ");
     scanf("%d", &n);
-    int arr[n]; 
+    int A[n]; 
     printf("Enter %d numbers:\n", n);
     for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+        scanf("%d", &A[i]);
     }
     printf("You entered: ");
     for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
+        printf("%d ", A[i]);
     }
     printf("\n");
     return 0;
